6bddfd1f673877d49751be691fa863918cda7c65
“Akshita0714” <akshitag1014@gmail.com>
2022-11-28 20:11:50 +0530

    Added an OpenAI API key by creating a .env file. All the frontend changes are in index.js and all the backend ones are in generate.js I didn't add two levels of generation since one gives a better kind of output.

---
+++ pages/api/generate.js
@@ -0,0 +1,25 @@
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+const basePromptPrefix = "Write me a scholarship essay for the given person on the given theme";
+const generateAction = async (req, res) => {
+  // Run first prompt
+  console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
+
+  const baseCompletion = await openai.createCompletion({
+    model: 'text-davinci-002',
+    prompt: `${basePromptPrefix}${req.body.userInput}`,
+    temperature: 0.85,
+    max_tokens: 3700,
+  });
+  
+  const basePromptOutput = baseCompletion.data.choices.pop();
+
+  res.status(200).json({ output: basePromptOutput });
+};
+
+export default generateAction;
